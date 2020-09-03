9cc007f106a4148b4d32fabf85f9e99527ea0a26
deman <demanlu@126.com>
2020-09-03 15:00:25 +0800

    reverse node

--- DataAndAlgorithm/app/src/main/java/com/demanmath/dataandalgorithm/data/linkedlist/LinkedListSolution.kt
+++ DataAndAlgorithm/app/src/main/java/com/demanmath/dataandalgorithm/data/linkedlist/LinkedListSolution.kt
@@ -11,14 +11,29 @@ import java.util.*
 class LinkedListSolution:ISolutionInterface {
 
     override fun run() {
-        var linkedList = LinkedList<Int>()
-        linkedList.initList()
-        linkedList.addNode(1)
-        linkedList.addNode(2)
-        linkedList.addNode(3)
-        linkedList.addNode(4)
-        linkedList.addNode(5)
-        reverseLinkedList(linkedList)
+//        val linkedList = LinkedList<Int>()
+//        linkedList.initList()
+//        linkedList.addNode(1)
+//        linkedList.addNode(2)
+//        linkedList.addNode(3)
+//        linkedList.addNode(4)
+//        linkedList.addNode(5)
+//        reverseLinkedList(linkedList)
+
+        val nodeHead = Node(1)
+        var i = 2
+        var temp = nodeHead
+        while(i<6){
+            var e = Node(i)
+            temp.next = e
+            e.next = null
+            temp = e
+            i++
+        }
+        TraceLog.i(nodeHead?.dumpList()?:"")
+        var res = reverseNode(nodeHead)
+        TraceLog.i(res?.dumpList()?:"")
+
     }
 
      private fun <T> reverseLinkedList(list: LinkedList<T>):LinkedList<T>{
@@ -39,4 +54,39 @@ class LinkedListSolution:ISolutionInterface {
          TraceLog.i(resultNode.dumpList())
          return resultNode
      }
+
+    private fun <T> reverseNode(head: Node<T>?):Node<T>?{
+        /*方法一，用stack的特性，存储链表的数据，然后反转*/
+        /**
+         * 如果链表为空时，代码是否能正常工作？
+         * 如果链表只包含一个结点时，代码是否能正常工作？
+         * 如果链表只包含两个结点时，代码是否能正常工作？
+         * 代码逻辑在处理头结点和尾结点的时候，是否能正常工作？
+         */
+        head?.let {p->
+            p.next?.let {
+                var stack = Stack<T>()
+                var temp:Node<T>? = head
+                while (temp!=null){
+                    stack.push(temp!!.value)
+                    temp = temp!!.next
+                }
+
+                var newHead = Node<T>(stack.pop())
+                var temp2 = newHead
+                while (!stack.isEmpty()){
+                    var e = Node(stack.pop())
+                    temp2.next = e
+                    e.next = null
+                    temp2 = e
+                }
+                return newHead
+
+            }?: kotlin.run {
+                return head
+            }
+        }?: kotlin.run {
+            return null
+        }
+    }
 }
--- DataAndAlgorithm/app/src/main/java/com/demanmath/dataandalgorithm/data/linkedlist/Node.kt
+++ DataAndAlgorithm/app/src/main/java/com/demanmath/dataandalgorithm/data/linkedlist/Node.kt
@@ -6,4 +6,15 @@ package com.demanmath.dataandalgorithm.data.linkedlist
 */
 class Node<T>(var value:T?) {
     var next:Node<T>? = null
+
+    fun dumpList():String{
+        var stringBuilder = StringBuilder()
+        stringBuilder.append("${this.value} ")
+        var temp = this.next
+        while (temp!=null){
+            stringBuilder.append("${temp.value} ")
+            temp = temp.next
+        }
+        return stringBuilder.toString()
+    }
 }
